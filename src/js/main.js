df8ad198616f36a8d9154880ad924b7a0f70d9a2
CarterTS <carterplasek@gmail.com>
2021-07-10 23:59:46 -0500

    Game Object Drawing

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,15 @@ var global_image = undefined;
 
 var game_state = "menu";
 
+var grid_size = 50;
+
+var game_objects = [{"pos": {"x":0, "y":0}}, {"pos": {"x":-1, "y":0}}, {"pos": {"x":1, "y":0}}];
+
 function draw()
 {
     if (typeof global_canvas === "undefined")
     {
-        global_canvas = document.getElementById("main_canvas")
+        global_canvas = document.getElementById("main_canvas");
     }
 
     if (typeof global_image === "undefined")
@@ -26,6 +30,9 @@ function draw()
 
     if (game_state === "in_game")
     {
+        global_canvas.width = global_canvas.offsetWidth;
+	    global_canvas.height = global_canvas.offsetHeight;
+
         var ctx = global_canvas.getContext("2d");
         
         draw_game(global_canvas, ctx);
@@ -36,6 +43,28 @@ function draw_game(canvas, ctx)
 {
     ctx.fillStyle = "blue";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    game_objects.forEach(function (item, index)
+    {
+        draw_game_object(item, canvas, ctx);
+    });
+}
+
+function grid_to_coord(grid_pos, canvas)
+{
+    return {"x": Math.round(canvas.width / 2) + grid_pos.x * grid_size, "y": Math.round(canvas.height / 2) + grid_pos.y * grid_size}
+}
+
+function draw_game_object(obj, canvas, ctx)
+{
+    ctx.fillStyle = "red";
+
+    width = 25;
+    height = 25;
+
+    pos = grid_to_coord(obj.pos, canvas);
+
+    ctx.fillRect(Math.round(pos.x - width / 2), Math.round(pos.y - height / 2), width, height);
 }
 
 function tick_game()
