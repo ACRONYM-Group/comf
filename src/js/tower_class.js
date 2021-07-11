92eaa2dde121f121cf9414feaa2eb3a032631d58
CarterTS <carterplasek@gmail.com>
2021-07-11 04:19:19 -0500

    Tracking

--- src/js/tower_class.js
+++ src/js/tower_class.js
@@ -13,12 +13,43 @@ class tower {
     tick()
     {
         var i = 0;
-        this.health *= 0.99;
+        // this.health *= 0.99;
 
         if (this.health < this.max_health * 0.01)
         {
             i = game_objects.indexOf(this);
             game_objects.splice(i, 1);
         }
+
+        // Check for the nearest enemy
+        let best = null;
+        let best_dist = null;
+
+        for (i in enemies)
+        {
+            let e = enemies[i];
+
+            let dist = Math.pow(this.x - e.x, 2.0) + Math.pow(this.y - e.y, 2.0);
+
+            if (best == null || dist < best_dist)
+            {
+                best = e;
+                best_dist = dist;
+            }
+        }
+
+        if (best != null)
+        {
+            var ctx = global_canvas.getContext("2d");
+
+            let p0 = grid_to_coord(this, global_canvas);
+            let p1 = grid_to_coord(best, global_canvas);
+
+            ctx.beginPath();
+            ctx.moveTo(p0.x, p0.y);
+            ctx.lineTo(p1.x, p1.y);
+            ctx.stroke();
+        }
+        
     }
 }
